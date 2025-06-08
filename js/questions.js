2c78b16d7f99cdc3ab57c0571f8ff9cd4b95dc46
vesrntechnologies <vennjoy@gmail.com>
2025-06-08 21:55:53 +0530

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,394 @@
+let questions = [
+  {
+    numb: 1,
+    question: "Which cement is more suitable for construction of abutment and piers?",
+    immg: "img1",
+    answer: "Modified port land cement",
+    options: [
+      "High alumina cement",
+      "Rapid hardening cement",
+      "Modified port land cement",
+      "Sulphate resisting cement"
+    ]
+  },
+  {
+    numb: 2,
+    question: "What is the name of the part labelled as X shown in the figure?",
+    immg: "img2",
+    answer: "Pith",
+    options: ["Pith", "Bast", "Sap wood", "Heart wood"]
+  },
+  {
+    numb: 3,
+    question: "What is the name of form labelled as X in bricks masonry?",
+    immg: "img3",
+    answer: "Queen closer",
+    options: ["Header", "Quoin closer", "Queen closer", "Quoin stretcher"]
+  },
+  {
+    numb: 4,
+    question: "Which type of paint is applied for iron work under water?",
+    immg: "img4",
+    answer: "Bituminous paint",
+    options: ["Asbestos paint", "Cellulose paint", "Aluminium paint", "Bituminous paint"]
+  },
+  {
+    numb: 5,
+    question: "What is the name of stone joint shown in the figure?",
+    immg: "img5",
+    answer: "Tabled joint",
+    options: ["Lap joint", "Butt joint", "Tabled joint", "Tongue and groove joint"]
+  },
+  {
+    numb: 6,
+    question: "What is the name of brick shown in the figure?",
+    immg: "img6",
+    answer: "King closer",
+    options: ["King closer", "Bevelled bat", "Queen closer", "Mitred closer"]
+  },
+  {
+    numb: 7,
+    question: "What is the name of tool used for setting angle in brick masonry?",
+    immg: "img7",
+    answer: "Bevel",
+    options: ["Bevel", "Plumb rule", "Masons square", "1 meter U folded rule"]
+  },
+  {
+    numb: 8,
+    question: "Which rigid board is known as pressed board?",
+    immg: "img8",
+    answer: "Fibre board",
+    options: ["Plywood board", "Compreg board", "Fibre board", "Laminated board"]
+  },
+  {
+    numb: 9,
+    question: "Which surfaces are recommended with cement paint and emulsion paint?",
+    immg: "img9",
+    answer: "Plastered surface",
+    options: ["Iron surface", "Plastered surface", "Wood surface", "Metal surface"]
+  },
+  {
+    numb: 10,
+    question: "What is the name of the term that in the formation of dark hard rings in the wood?",
+    immg: "img10",
+    answer: "Knotting",
+    options: ["Finishing", "Knotting", "Stopping", "Cleaning"]
+  },
+  {
+    numb: 11,
+    question: "Which metal consists of copper and tin?",
+    immg: "img11",
+    answer: "Bronze",
+    options: ["Bronze", "Brass", "Dow metal", "Nickel silver"]
+  },
+  {
+    numb: 12,
+    question: "What is the part marked as X shown in the figure?",
+    immg: "img12",
+    answer: "Coping",
+    options: ["Coping", "Parapet", "Throating", "D.P.C"]
+  },
+  {
+    numb: 13,
+    question: "What is the name of brick shown in the figure?",
+    immg: "img13",
+    answer: "Perforated clay brick",
+    options: ["Cellular clay brick", "Hollow clay brick", "Hollow concrete brick", "Perforated clay brick"]
+  },
+  {
+    numb: 14,
+    question: "What is the process of surface smoothened by rubbing with sand paper or pumice stone for varnishing?",
+    immg: "img14",
+    answer: "Preparation of surface",
+    options: [
+      "Application of surface",
+      "Preparation of surface",
+      "Cleaning of surface",
+      "Finishing of surface"
+    ]
+  },
+  {
+    numb: 15,
+    question: "Which masonry has provision for clamps or dowels between facing and backing of wall?",
+    immg: "img15",
+    answer: "Composite masonry",
+    options: [
+      "Stone masonry",
+      "Brick masonry",
+      "Brick masonry in mud mortar",
+      "Composite masonry"
+    ]
+  },
+  {
+    numb: 16,
+    question: "What is the stone masonry shown in the figure?",
+    immg: "img16",
+    answer: "Uncoursed rubble masonry",
+    options: [
+      "Coursed rubble masonry",
+      "Coursed rubble masonry 1st sort",
+      "Coursed rubble masonry 2nd sort",
+      "Uncoursed rubble masonry"
+    ]
+  },
+  {
+    numb: 17,
+    question: "What is the part marked as X shown in the figure?",
+    immg: "img17",
+    answer: "Frieze",
+    options: ["Corbel", "Blocking", "String", "Frieze"]
+  },
+  {
+    numb: 18,
+    question: "What is marked as X in brickwork shown in the figure?",
+    immg: "img18",
+    answer: "Cornice brick",
+    options: ["Cornice brick", "Bullnose brick", "Cant brick", "Round ended brick"]
+  },
+  {
+    numb: 19,
+    question: "What is the name of the wall?",
+    immg: "img19",
+    answer: "Stone facing with rubble backing",
+    options: [
+      "Stone facing with rubble backing",
+      "Ashlar facing with rubble backing",
+      "Brick facing with concrete backing",
+      "Facing of brick works and backing of ashlar masonry"
+    ]
+  },
+  {
+    numb: 20,
+    question: "What is the process of removing moisture present in freshly felled timber?",
+    immg: "img20",
+    answer: "Seasoning",
+    options: ["Hardening", "Stiffing", "Sapping", "Seasoning"]
+  },
+  
+    {
+      numb: 21,
+      question: "What is the artificial seasoning while timber immersed in a solution of suitable salt?",
+      immg: "img21",
+      answer: "Chemical seasoning",
+      options: ["Chemical seasoning", "Electrical seasoning", "Water seasoning", "Kiln seasoning"]
+    },
+    {
+      numb: 22,
+      question: "What is the defect due to seasoning in timber shown in figure?",
+      immg: "img22",
+      answer: "Cup",
+      options: ["Cup", "Bow", "Split", "Warp"]
+    },
+    {
+      numb: 23,
+      question: "What is the use of thin sheets of wood varies from 0.4 mm to 6 mm thickness?",
+      immg: "img23",
+      answer: "Veneers",
+      options: ["Plywood", "Fibre board", "Laminated board", "Veneers"]
+    },
+    {
+      numb: 24,
+      question: "What is the part marked as X in masonry shown in figure?",
+      immg: "img24",
+      answer: "Header",
+      options: ["Header", "Quoin", "Bed joint", "Stretcher"]
+    },
+    {
+      numb: 25,
+      question: "What is the part marked as X shown in figure?",
+      immg: "img25",
+      answer: "Stretcher",
+      options: ["Queen closer", "Stretcher", "Header", "Quoin"]
+    },
+    {
+      numb: 26,
+      question: "What is marked as X in masonry shown in figure?",
+      immg: "img26",
+      answer: "Bed joint",
+      options: ["Stretcher", "Bed joint", "Quoin closer", "Quoin"]
+    },
+    {
+      numb: 27,
+      question: "What is the stone joint used in stone masonry shown in figure?",
+      immg: "img27",
+      answer: "Rusticated joint",
+      options: ["Plugged joint", "Rusticated joint", "Tabled joint", "Butt joint"]
+    },
+    {
+      numb: 28,
+      question: "What is the stone joint used in stone masonry shown in figure?",
+      immg: "img28",
+      answer: "Saddled joint",
+      options: ["Saddled joint", "Butt joint", "Tabled joint", "Plugged joint"]
+    },
+    {
+      numb: 29,
+      question: "Which stone joint is used in arch work?",
+      immg: "img29",
+      answer: "Rebated joint",
+      options: ["Butt joint", "Table joint", "Rebated joint", "Joggled joint"]
+    },
+    {
+      numb: 30,
+      question: "What is the name of masonry shown in figure?",
+      immg: "img30",
+      answer: "Random rubble masonry",
+      options: [
+        "Dry rubble masonry",
+        "Random rubble masonry",
+        "Coursed rubble masonry",
+        "Coursed rubble masonry 1st sort"
+      ]
+    },
+    {
+      numb: 31,
+      question: "Which stone joint is used for coping cornice?",
+      immg: "img31",
+      answer: "Cramped joint",
+      options: ["Tabled joint", "Plugged joint", "Joggled joint", "Cramped joint"]
+    },
+    {
+      numb: 32,
+      question: "What is the name of bond used in masonry shown in figure?",
+      immg: "img32",
+      answer: "Facing bond",
+      options: ["English cross wall bond", "Dutch bond", "Facing bond", "Racking bond"]
+    },
+    {
+      numb: 33,
+      question: "What type of board is used for the construction of bus bodies and marine?",
+      immg: "img33",
+      answer: "Block board",
+      options: ["Laminated board", "Fibre board", "Plywood", "Block board"]
+    },
+    {
+      numb: 34,
+      question: "What is the name of timber with parallel sides having thickness less than 50 mm and its width exceeds 50 mm?",
+      immg: "img34",
+      answer: "Plank",
+      options: ["Plank", "Pole", "Deal", "Baulk"]
+    },
+    {
+      numb: 35,
+      question: "Which masonry has regular square or rectangular shaped stones with accurate bed joints?",
+      immg: "img35",
+      answer: "Ashlar masonry",
+      options: [
+        "Coursed R.B",
+        "Uncoursed Rubble masonry",
+        "Ashlar masonry",
+        "Flint rubble masonry"
+      ]
+    },
+    {
+      numb: 36,
+      question: "What is the name of cup shakes that covers the entire ring?",
+      immg: "img36",
+      answer: "Ring shakes",
+      options: ["Heart shakes", "Star shakes", "Radial shakes", "Ring shakes"]
+    },
+    {
+      numb: 37,
+      question: "What is the process of cutting down the trees?",
+      immg: "img37",
+      answer: "Felling",
+      options: ["Felling", "Warping", "Seasoning", "Barking"]
+    },
+    {
+      numb: 38,
+      question: "Which process removes the moisture or sap that is present in a freshly felled timber?",
+      immg: "img38",
+      answer: "Seasoning",
+      options: ["Raping", "Boiling", "Seasoning", "Kilning"]
+    },
+    {
+      numb: 39,
+      question: "What wall is built around the flat roof for protection?",
+      immg: "img39",
+      answer: "Parapet",
+      options: ["Coping", "Parapet", "Roof", "Lintel"]
+    },
+    {
+      numb: 40,
+      question: "What is the dead load?",
+      immg: "img40",
+      answer: "Roof",
+      options: ["Roof", "Snow", "Wind pressure", "Men and animals"]
+    },
+    {
+      numb: 41,
+      question: "What is the name of tool shown in figure?",
+      immg: "img41",
+      answer: "Post-hole auger",
+      options: ["Shell auger", "Screw auger", "Post-hole auger", "Wash boring auger"]
+    },
+    {
+      numb: 42,
+      question: "What is the name of foundation shown in figure?",
+      immg: "img42",
+      answer: "Trapezoidal footing",
+      options: ["Raft footing", "Strap footing", "Rectangular footing", "Trapezoidal footing"]
+    },
+    {
+      numb: 43,
+      question: "What is the name of the foundation that is due to heavy inflow seepage and not possible to excavate the trenches and keep them dry?",
+      immg: "img43",
+      answer: "Pile foundation",
+      options: ["Pile foundation", "Raft foundation", "Spread foundation", "Shallow foundation"]
+    },
+    {
+      numb: 44,
+      question: "What is the name of the piles that are driven at an inclination to resist large horizontal or inclined forces?",
+      immg: "img44",
+      answer: "Batter piles",
+      options: ["Batter piles", "Sheet piles", "Anchor piles", "Fender piles"]
+    },
+    {
+      numb: 45,
+      question: "What is the value for specific heat of water?",
+      immg: "img45",
+      answer: "1",
+      options: ["4", "3", "2", "1"]
+    },
+    {
+      numb: 46,
+      question: "At what temperature will Fahrenheit and centigrade thermometers give the same reading?",
+      immg: "img46",
+      answer: "-40°C",
+      options: ["-38°C", "-39°C", "-40°C", "-41°C"]
+    },
+    {
+      numb: 47,
+      question: "What is the boiling point of water?",
+      immg: "img47",
+      answer: "100°C",
+      options: ["0°C", "32°C", "100°C", "212°C"]
+    },
+    {
+      numb: 48,
+      question: "What is the side of a square whose area is 625mm²?",
+      immg: "img48",
+      answer: "25 mm",
+      options: ["15 mm", "20 mm", "25 mm", "30 mm"]
+    },
+    {
+      numb: 49,
+      question: "What is the area of a rectangle, whose length and breadth are 10cm and 8cm respectively?",
+      immg: "img49",
+      answer: "80 cm²",
+      options: ["75 cm²", "80 cm²", "85 cm²", "90 cm²"]
+    },
+    {
+      numb: 50,
+      question: "Which is equal to sinθ?",
+      immg: "img50",
+      answer: "Opposite/Hypotenuse",
+      options: [
+        "Opposite/Adjacent",
+        "Adjacent/Hypotenuse",
+        "Opposite/Hypotenuse",
+        "1/Cosecθ"
+      ]
+    }
+  
+];
